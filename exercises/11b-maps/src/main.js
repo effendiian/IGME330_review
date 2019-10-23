96ed2f166119c02ed5d4ea74cfe7dfc1f6b8c18f
Ian Effendi <effendiian@gmail.com>
2019-10-23 17:24:40 -0400

    update the map tutorial assignment

---
+++ exercises/11b-maps/src/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
+}
+
+function addSecretAPIKey() {
+
+}
+
+window.onload = initMap;
